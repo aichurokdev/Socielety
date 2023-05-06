a583e90042a328e7ef137ed0facdfda096735273
aichurokdev <aichurokkuttubekovna1@gmail.com>
2023-05-06 14:24:27 +0300

    frist commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// const swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 6,
+//     // spaceBetween: 30,
+//     freeMode: true,
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+// });
